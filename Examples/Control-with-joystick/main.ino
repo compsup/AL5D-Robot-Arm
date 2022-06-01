f8291f3221db5944a73237598963ae60e4b00bd3
Compsup <duncanmcquat@gmail.com>
2022-05-31 22:08:31 -0400

    Fixed directions, fixed controller serial bug

--- Examples/Control-with-joystick/main.ino
+++ Examples/Control-with-joystick/main.ino
@@ -1,8 +1,8 @@
 // Arduino pin numbers
 const int left_SW_pin = 7; // digital pin connected to switch output
-const int left_X_pin = A0; // analog pin connected to X output
-const int left_Y_pin = A1; // analog pin connected to Y output
-int left_base_x = 0;
+const int left_x_pin = A0; // analog pin connected to X output
+const int left_y_pin = A1; // analog pin connected to Y output
+int left_base_y = 0;
 
 #include <SoftwareSerial.h>
 
@@ -10,12 +10,13 @@ int left_base_x = 0;
 SoftwareSerial ssc32u(12, 13);
 
 void setup() {
-    ssc32u.listen();
     pinMode(left_SW_pin, INPUT);
     digitalWrite(left_SW_pin, HIGH);
+    ssc32u.begin(9600);
+    ssc32u.listen();
     Serial.begin(9600);
     ssc32u.write("#0P1500 #1P1500 #2P1500 #3P1500 #4P1500 #5P1500 #6P1500\r");
-    left_base_x = analogRead(left_X_pin);
+    left_base_y = analogRead(left_y_pin);
 }
 int8_t step0 = 0;
 uint16_t pulse = 1500;
@@ -27,17 +28,17 @@ uint16_t pulse5 = 1500;
 
 void loop() {
     
-    step0 = (analogRead(left_X_pin) - left_base_x) * 0.02;
+    step0 = (analogRead(left_y_pin) - left_base_y) * 0.02;
     pulse += step0;
 
     Serial.print("Pulse: ");
     Serial.println(pulse);
     Serial.print("Step: ");
     Serial.println(step0);
-    Serial.print("left_base_x: ");
-    Serial.println(left_base_x);
-    Serial.print("X_val: ");
-    Serial.println(analogRead(left_X_pin));
+    Serial.print("left_base_y: ");
+    Serial.println(left_base_y);
+    Serial.print("y_val: ");
+    Serial.println(analogRead(left_y_pin));
     // Motor instructions are as follows
     // #<motor_num>P<pulse_value>\r
     char output[80];
