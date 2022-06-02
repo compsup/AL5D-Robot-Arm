134366e5309971cd1ac1341b96867098bf935acd
XTheMoose <tbdakers@icloud.com>
2022-06-02 09:31:55 -0700

    Cleaned code, remove unused variable, add comments

--- Examples/Control-with-joystick/main.ino
+++ Examples/Control-with-joystick/main.ino
@@ -7,14 +7,13 @@ const int left_y_pin = A1;
 const int right_SW_pin = 7;
 const int right_x_pin = A5;
 const int right_y_pin = A4;
-#define CLK 2
-#define DT 3
-#define SW 4
+const int CLK = 2;
+const int DT = 3;
+const int SW = 4;
 
-int counter = 0;
+//Encoder Setup
 int currentStateCLK;
 int lastStateCLK;
-String currentDir = "";
 unsigned long lastButtonPress = 0;
 
 // Offsets for joystick deadzone - get's setup at startup
@@ -26,28 +25,28 @@ SoftwareSerial ssc32u(12, 13);
 
 void setup()
 {
+    // Pin layouts
     pinMode(left_SW_pin, INPUT);
     digitalWrite(left_SW_pin, HIGH);
-
-    Serial.begin(9600);
-    Serial.println("===> System Init <===\n");
-
-    Serial.print("Connecting too SSC-32U...");
-    ssc32u.begin(9600);
-    ssc32u.listen();
-    Serial.println(" Done");
-
-    // Set encoder pins as inputs
     pinMode(CLK, INPUT);
     pinMode(DT, INPUT);
     pinMode(SW, INPUT_PULLUP);
+
+    // Serial Setup
+    Serial.begin(9600);
+    Serial.println("===> System Init <===\n");
+    Serial.print("Connecting too SSC-32U...");
+    // ssc32u Controller Setup
+    ssc32u.begin(9600);
+    ssc32u.listen();
+    Serial.println(" Done");
 
     // Read the initial state of CLK
     lastStateCLK = digitalRead(CLK);
 
     // Center servos
     Serial.print("Centering Servos...");
-    ssc32u.write("#0P0 #1P0 #2P0 #3P0 #4P0 #5P0 #6P0\r");
+    ssc32u.write("#0P1500 #1P1500 #2P1500 #3P1500 #4P1500 #5P1500 #6P1500\r");
     Serial.println(" Done");
     Serial.print("Calibrating Joysticks...");
     left_base_y = analogRead(left_y_pin);
@@ -63,15 +62,15 @@ void setup()
     attachInterrupt(1, updateEncoder, CHANGE);
 }
 
+// Define arrays for servo values, maxes, and minimums
 uint16_t pulses[6] = {1500, 1500, 1500, 1500, 1500, 1500};
 uint16_t max[6] = {2500, 2500, 2500, 2500, 2500, 2500};
 uint16_t min[6] = {500, 500, 500, 500, 500, 500};
 
 void loop()
 {
-    // Read the button state
+    // Reset Servos to Base values when Motor-Encoder is pressed
     int btnState = digitalRead(SW);
-
     // If we detect LOW signal, button is pressed
     if (btnState == LOW)
     {
@@ -84,10 +83,10 @@ void loop()
                 pulses[i] = 1500;
             }
         }
-
         // Remember last button press event
         lastButtonPress = millis();
     }
+
     // Formula to give us a proper step value too increase or decrease the pulse by.
     pulses[0] -= (analogRead(left_y_pin) - left_base_y) / 15;   // Base
     pulses[1] += (analogRead(left_x_pin) - left_base_x) / 25;   // Shoulder
@@ -109,11 +108,14 @@ void loop()
 
     // Motor instructions are as follows, note: S and T are optional
     // #<servo_num>P<pulse>S<speed>T<time>\r
+    // note: if time is at the end of the statement then it will effect all servos
+    // #0P1500 #1P1500 #2P1500 T100
     char output[80];
-    sprintf(output, "#0P%uT100 #1P%uT100 #2P%uT100 #3P%uT100 #4P%u #5P%u\r", pulses[0], pulses[1], pulses[2], pulses[3], pulses[4], pulses[5]);
+    sprintf(output, "#0P%u #1P%u #2P%u #3P%uT100 #4P%u #5P%u\r", pulses[0], pulses[1], pulses[2], pulses[3], pulses[4], pulses[5]);
     ssc32u.write(output);
 }
 
+//Servo #5 Pulse Formula. Changed when the encoder is rotated
 void updateEncoder()
 {
     // Read the current state of CLK
@@ -135,13 +137,7 @@ void updateEncoder()
             // Encoder is rotating CW so increment
             pulses[5] -= 100;
         }
-
-        Serial.print("Direction: ");
-        Serial.print(currentDir);
-        Serial.print(" | Counter: ");
-        Serial.println(counter);
     }
-
     // Remember last CLK state
     lastStateCLK = currentStateCLK;
 }
