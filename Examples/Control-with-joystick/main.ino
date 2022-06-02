9454c574851faa6a87714ca59046ecce7b2c790c
XTheMoose <tbdakers@icloud.com>
2022-06-02 10:15:45 -0400

    Added Wrist Rotation

--- Examples/Control-with-joystick/main.ino
+++ Examples/Control-with-joystick/main.ino
@@ -7,6 +7,15 @@ const int left_y_pin = A1;
 const int right_SW_pin = 7;
 const int right_x_pin = A5;
 const int right_y_pin = A4;
+#define CLK 2
+#define DT 3
+#define SW 4
+
+int counter = 0;
+int currentStateCLK;
+int lastStateCLK;
+String currentDir = "";
+unsigned long lastButtonPress = 0;
 
 // Offsets for joystick deadzone - get's setup at startup
 int left_base_y, left_base_x;
@@ -28,9 +37,17 @@ void setup()
     ssc32u.listen();
     Serial.println(" Done");
 
+    // Set encoder pins as inputs
+    pinMode(CLK, INPUT);
+    pinMode(DT, INPUT);
+    pinMode(SW, INPUT_PULLUP);
+
+    // Read the initial state of CLK
+    lastStateCLK = digitalRead(CLK);
+
     // Center servos
     Serial.print("Centering Servos...");
-    ssc32u.write("#0P1500 #1P1500 #2P1500 #3P1500 #4P1500 #5P1500 #6P1500\r");
+    ssc32u.write("#0P0 #1P0 #2P0 #3P0 #4P0 #5P0 #6P0\r");
     Serial.println(" Done");
     Serial.print("Calibrating Joysticks...");
     left_base_y = analogRead(left_y_pin);
@@ -39,6 +56,11 @@ void setup()
     right_base_x = analogRead(right_x_pin);
     Serial.println(" Done\n");
     Serial.println("===> Starting Main Loop <===");
+
+    // Call updateEncoder() when any high/low changed seen
+    // on interrupt 0 (pin 2), or interrupt 1 (pin 3)
+    attachInterrupt(0, updateEncoder, CHANGE);
+    attachInterrupt(1, updateEncoder, CHANGE);
 }
 
 uint16_t pulses[6] = {1500, 1500, 1500, 1500, 1500, 1500};
@@ -47,6 +69,25 @@ uint16_t min[6] = {500, 500, 500, 500, 500, 500};
 
 void loop()
 {
+    // Read the button state
+    int btnState = digitalRead(SW);
+
+    // If we detect LOW signal, button is pressed
+    if (btnState == LOW)
+    {
+        // if 50ms have passed since last LOW pulse, it means that the
+        // button has been pressed, released and pressed again
+        if (millis() - lastButtonPress > 50)
+        {
+            for (uint8_t i = 0; i < 6; i++)
+            {
+                pulses[i] = 1500;
+            }
+        }
+
+        // Remember last button press event
+        lastButtonPress = millis();
+    }
     // Formula to give us a proper step value too increase or decrease the pulse by.
     pulses[0] -= (analogRead(left_y_pin) - left_base_y) / 15;   // Base
     pulses[1] += (analogRead(left_x_pin) - left_base_x) / 25;   // Shoulder
@@ -67,8 +108,40 @@ void loop()
     }
 
     // Motor instructions are as follows, note: S and T are optional
-    // #<servo_num>P<pulse>S<speed>T<time>\r     
+    // #<servo_num>P<pulse>S<speed>T<time>\r
     char output[80];
-    sprintf(output, "#0P%u #1P%u #2P%u #3P%u #4P%u #5P%u\r", pulses[0], pulses[1], pulses[2], pulses[3], pulses[4], pulses[5]);
+    sprintf(output, "#0P%uT100 #1P%uT100 #2P%uT100 #3P%uT100 #4P%u #5P%u\r", pulses[0], pulses[1], pulses[2], pulses[3], pulses[4], pulses[5]);
     ssc32u.write(output);
+}
+
+void updateEncoder()
+{
+    // Read the current state of CLK
+    currentStateCLK = digitalRead(CLK);
+
+    // If last and current state of CLK are different, then pulse occurred
+    // React to only 1 state change to avoid double count
+    if (currentStateCLK != lastStateCLK && currentStateCLK == 1)
+    {
+
+        // If the DT state is different than the CLK state then
+        // the encoder is rotating CCW so decrement
+        if (digitalRead(DT) != currentStateCLK)
+        {
+            pulses[5] += 100;
+        }
+        else
+        {
+            // Encoder is rotating CW so increment
+            pulses[5] -= 100;
+        }
+
+        Serial.print("Direction: ");
+        Serial.print(currentDir);
+        Serial.print(" | Counter: ");
+        Serial.println(counter);
+    }
+
+    // Remember last CLK state
+    lastStateCLK = currentStateCLK;
 }
