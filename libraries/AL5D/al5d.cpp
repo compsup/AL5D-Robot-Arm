7e6a8d146c1cd27c3918f3ebc1705e4d64036755
XTheMoose <tbdakers@icloud.com>
2022-06-07 13:43:17 -0400

    example program for library

--- libraries/AL5D/al5d.cpp
+++ libraries/AL5D/al5d.cpp
@@ -18,6 +18,7 @@ void al5d::set_motor(uint8_t motor_num, int degree, int time, int speed)
     {
         pulse = 2500;
     }
+    motor_locs[motor_num] = degree;
     char output[80];
     sprintf(output, "#%uP%uS%uT%u\r", motor_num, pulse, speed, time);
     ssc32u.write(output);
@@ -33,6 +34,11 @@ void al5d::set_all_motors(int degree, int time, int speed)
     {
         pulse = 2500;
     }
+    for (int i = 0; i < 6; i++)
+    {
+        motor_locs[i] = degree;
+    }
+
     char output[80];
     sprintf(output, "#0P%uS%u #1P%uS%u #2P%uS%u #33P%uS%u #4P%uS%u #5P%uS%uT%u\r",
             pulse, speed,
@@ -47,4 +53,8 @@ void al5d::set_all_motors(int degree, int time, int speed)
 void al5d::center_motors()
 {
     ssc32u.write("#0P1500 #1P1500 #2P1500 #3P1500 #4P1500 #5P1500 #6P1500\r");
+}
+int al5d::get_motor_loc(uint8_t motor_num)
+{
+    return motor_locs[motor_num];
 }
--- libraries/AL5D/al5d.h
+++ libraries/AL5D/al5d.h
@@ -9,5 +9,9 @@ public:
     void set_motor(uint8_t motor_num, int degree, int time = 500, int speed = 750);
     void set_all_motors(int degree, int time = 500, int speed = 750);
     void center_motors();
+    int get_motor_loc(uint8_t motor_num);
+
+private:
+    uint16_t motor_locs[6] = {0, 0, 0, 0, 0, 0};
 };
 #endif
