47351d502219d255ca42c468355b44adcdb1c97c
Tyler Dakers <tbdakers@icloud.com>
2023-03-27 11:11:47 -0400

    Update rotary-v2.ino
    First working version. Includes description at beginning of code. Uses toggle method to include up to 20 specific commands.

--- Examples/Rotary-Phone/rotary-v2/rotary-v2.ino
+++ Examples/Rotary-Phone/rotary-v2/rotary-v2.ino
@@ -1,9 +1,38 @@
+// This program utilizes a rotary phone to execute functions on the AL5D robotic arm.
+// The instructions are as follows:
+
+/* On Startup:
+1 = Select Base (1)
+2 = Select Shoulder (2)
+3 = Select Elbow (3)
+4 = Select Vertical Wrist (4)
+5 = Select Grip (5)
+6 = Select Rotate Wrist (6)
+7 = Increase Motor Speed
+8 = Decrease Motor Speed
+9 = Reset all Motors
+*/
+
+/* When Motor Selection occurs, a new set of commands is availible to increment the degree of the servos:
+1 = Increase by 5
+2 = Decrease by 5
+3 = Increase by 10
+4 = Decrease by 10
+5 = Increase by 20
+6 = Decrease by 20
+7/8 = Increment Speed
+9 = Reset Current Motor
+10 = Deselect Motor
+* once a command has been executed, the process will repeat
+
+Example - Input: 26 = Decrease Degree of Motor 2 by 20 Degrees
+*/
+
 #include <al5d.h>
 
 const int input = 8;
-int pulse = 0;
+uint8_t pulse = 0;
 int var = 0;
-
 int timer = 0;
 
 al5d AL5D;
@@ -12,15 +41,17 @@ void setup()
 {
   pinMode(input,INPUT);
   Serial.begin(9600);
-  Serial.println(F("No pulses yet..."));
-
   // Robotic Arm Startup
   AL5D.begin();
+  Serial.println("******** Setup Complete *********");
 }
 
 int8_t degree;
 uint8_t num;
-int8_t speed = 100;
+int time = 500;
+
+int toggle = 0;
+
 
 void loop() 
 {
@@ -34,34 +65,84 @@ void loop()
     timer = 0;
   }
 
-  if (pulse == 8 && timer == 50) //7 Dialed
+  if (pulse > 1 && timer == 10 )
   {
-    speed += 
-  }
-    else if (pulse == 9 && timer == 50) //8 Dialed
-  {
-    num = 4;
-    degree = -75;
-  }
-    else if (pulse == 10 && timer == 50) //9 Dialed
-  {
-    num = 4;
-    degree = -75;
-  }
-    else if (pulse == 11 && timer == 50) //0 Dialed
-  {
-    num = 4;
-    degree = -75;
+
+    
+    if (pulse == 8) //7 Dialed
+    {
+      if ( time > 0 )
+      {
+        time -= 500;
+      }
+      
+    }
+
+    else if (pulse == 9) //8 Dialed
+    {
+      time += 500;
+    }
+
+    else if (toggle == 1) //Dials will move motors x degrees
+    {
+      if (pulse == 2) //1 Dialed
+      {
+        degree = AL5D.get_motor_loc(num) + 5;
+      }
+      else if (pulse == 3 ) //2 Dialed
+      {
+        degree = AL5D.get_motor_loc(num) - 5;
+      }
+      else if (pulse == 4 ) //3 Dialed
+      {
+        degree = AL5D.get_motor_loc(num) + 10;
+      }
+      else if (pulse == 5 ) //4 Dialed
+      {
+        degree = AL5D.get_motor_loc(num) - 10;
+      }
+      else if (pulse == 6) //5 Dialed
+      {
+        degree = AL5D.get_motor_loc(num) + 20;
+      }
+      else if (pulse == 7) //6 Dialed
+      {
+        degree = AL5D.get_motor_loc(num) - 20;
+      }
+      else if (pulse == 10) //9 Dialed
+      {
+        degree = 0;
+      }
+      else if (pulse == 11) {} //0 Dialed
+      toggle = 0;  
+       
+    }
+
+  else //Dials will select motor to move
+    {
+      if (pulse == 10) //9 Dialed
+      {
+        AL5D.center_motors();
+        degree = 0;
+      }
+      else if (pulse == 11) {}
+      else
+      {
+        num = 0;
+        num = pulse - 2;   
+        toggle = 1; 
+        degree = AL5D.get_motor_loc(num);   
+      }
+    }
+    
   }
 
-  move_motor(var);
- 
-  AL5D.set_motor(num, degree, speed);
+  AL5D.set_motor(num, degree, time);
+  
 
   // Write Determined Pulse Value
-    if(pulse > 1 && timer == 50)
+  if(pulse > 1 && timer == 10)
   {
-    Serial.print(pulse - 1);
     pulse = 1;
   }
 
@@ -71,44 +152,12 @@ void loop()
     var=0;
   }
 
-  // Reset Timer after 5 seconds
+  // Reset Timer after 1 seconds
   if(timer==1000)
   {
     pulse = 1;
     timer = 0;
   }
-
   delay(5);
-}
-
-void move_motor(uint8_t num)
-{
-  num --;
-
-    if (pulse == 2 && timer == 50) //1 Dialed
-    {
-      degree = 5;
-    }
-    else if (pulse == 3 && timer == 50) //2 Dialed
-    {
-      degree = -5;
-    }
-    else if (pulse == 4 && timer == 50) //3 Dialed
-    {
-      degree = 10;
-    }
-    else if (pulse == 5 && timer == 50) //4 Dialed
-    {
-      degree = -10;
-    }
-    else if (pulse == 6 && timer == 50) //5 Dialed
-    {
-      degree = 20;
-    }
-    else if (pulse == 7 && timer == 50) //6 Dialed
-    {
-      degree = -20;
-    }
-
-
+  
 }
